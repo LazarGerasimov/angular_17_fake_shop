eccb4513de6e3a93076a3bd52f897eaa564812e2
Laz <lazgerasimov@gmail.com>
2024-01-12 18:25:23 +0200

    add product header comp with dropdown menu scaff

---
+++ src/app/pages/home/components/products-header/products-header.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+
+@Component({
+  selector: 'app-products-header',
+  standalone: true,
+  imports: [MatCardModule, MatButtonModule, MatMenuModule],
+  templateUrl: './products-header.component.html',
+  styleUrl: './products-header.component.scss',
+})
+export class ProductsHeaderComponent {}
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { ProductsHeaderComponent } from './components/products-header/products-header.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [MatSidenavModule],
+  imports: [MatSidenavModule, ProductsHeaderComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
