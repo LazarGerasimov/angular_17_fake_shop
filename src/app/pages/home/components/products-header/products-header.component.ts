908d269c9e51b7f3aa83858455958488fccac416
Laz <lazgerasimov@gmail.com>
2024-01-12 19:01:32 +0200

    update products header items count dropdown

--- src/app/pages/home/components/products-header/products-header.component.ts
+++ src/app/pages/home/components/products-header/products-header.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-products-header',
   standalone: true,
-  imports: [MatCardModule, MatButtonModule, MatMenuModule],
+  imports: [MatCardModule, MatButtonModule, MatMenuModule, MatIconModule],
   templateUrl: './products-header.component.html',
   styleUrl: './products-header.component.scss',
 })
@@ -15,6 +16,10 @@ export class ProductsHeaderComponent {
   itemsShowCount = 12;
 
   onSortUpdated(newSort: string) {
-    this.sort = newSort
+    this.sort = newSort;
+  }
+
+  onItemsUpdated(count: number) {
+    this.itemsShowCount = count
   }
 }
