2d147b93a227329a35784f094391eed88b8cfac6
Laz <lazgerasimov@gmail.com>
2024-01-13 17:42:54 +0200

    upd cart data table

--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { MatTableModule } from '@angular/material/table';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 import { CartInterface, CartItemInterface } from '../../models/cart.model';
 import { CommonModule } from '@angular/common';
@@ -16,6 +17,7 @@ import { RouterModule } from '@angular/router';
     CommonModule,
     MatButtonModule,
     RouterModule,
+    MatIconModule,
   ],
   templateUrl: './cart.component.html',
   styleUrl: './cart.component.scss',
@@ -28,6 +30,13 @@ export class CartComponent implements OnInit {
         name: 'Nike Shoes',
         price: 25,
         quantity: 3,
+        id: '1',
+      },
+      {
+        product: 'https://via.placeholder.com/150',
+        name: 'Nike Shoes',
+        price: 30,
+        quantity: 4,
         id: '1',
       },
     ],
@@ -47,5 +56,11 @@ export class CartComponent implements OnInit {
 
   ngOnInit(): void {
     this.dataSource = this.cart.items;
+  }
+
+  getTotal(items: CartItemInterface[]): number {
+    return items
+      .map((item) => item.price * item.quantity)
+      .reduce((prev, current) => prev + current, 0);
   }
 }
