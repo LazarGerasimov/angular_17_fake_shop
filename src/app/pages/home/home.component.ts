bc772272b0aa1a17d8aac1a0c5efe58051160547
Laz <lazgerasimov@gmail.com>
2024-01-13 14:40:31 +0200

    add product-box scaffold

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,26 +2,39 @@ import { Component } from '@angular/core';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { ProductsHeaderComponent } from './components/products-header/products-header.component';
 import { FiltersComponent } from './components/filters/filters.component';
+import { MatGridListModule } from '@angular/material/grid-list';
+
+const ROWS_HEIGHT: { [id: number]: number } = {
+  1: 400,
+  3: 335,
+  4: 350,
+};
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [MatSidenavModule, ProductsHeaderComponent, FiltersComponent,],
+  imports: [
+    MatSidenavModule,
+    ProductsHeaderComponent,
+    FiltersComponent,
+    MatGridListModule,
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
 export class HomeComponent {
-
-  public cols = 3; // default 
+  public cols = 3; // default
+  public rowHeight = ROWS_HEIGHT[this.cols];
   public category: string | undefined;
 
   onColumnsCountChange(colsNum: number) {
     this.cols = colsNum;
     // console.log(this.cols)
+    this.rowHeight = ROWS_HEIGHT[this.cols];
   }
 
   onShowCategory(newCategory: string) {
     this.category = newCategory;
-    console.log(this.category)
+    console.log(this.category);
   }
 }
