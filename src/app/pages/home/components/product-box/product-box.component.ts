aecbf1ccb973b81138f3d5b2a78e884f759b3918
Laz <lazgerasimov@gmail.com>
2024-01-13 18:34:26 +0200

    implement add to cart logic

--- src/app/pages/home/components/product-box/product-box.component.ts
+++ src/app/pages/home/components/product-box/product-box.component.ts
@@ -1,7 +1,8 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
+import { ProductInterface } from '../../../../models/product.model';
 
 @Component({
   selector: 'app-product-box',
@@ -12,4 +13,20 @@ import { MatIconModule } from '@angular/material/icon';
 })
 export class ProductBoxComponent {
   @Input() fullWidthMode = false;
+
+  dummyProduct: ProductInterface | undefined = {
+    id: 1,
+    title: 'test title',
+    price: 75,
+    category: 'shoes',
+    description: 'test description',
+    image: 'https://via.placeholder.com/150',
+  };
+
+  // add event emitter
+  @Output() addToCart = new EventEmitter();
+
+  onAddToCart() {
+    this.addToCart.emit(this.dummyProduct);
+  }
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { ProductsHeaderComponent } from './components/products-header/products-header.component';
 import { FiltersComponent } from './components/filters/filters.component';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { ProductBoxComponent } from './components/product-box/product-box.component';
+import { CartService } from '../../services/cart.service';
+import { ProductInterface } from '../../models/product.model';
 
 const ROWS_HEIGHT: { [id: number]: number } = {
   1: 400,
@@ -19,7 +21,7 @@ const ROWS_HEIGHT: { [id: number]: number } = {
     ProductsHeaderComponent,
     FiltersComponent,
     MatGridListModule,
-    ProductBoxComponent
+    ProductBoxComponent,
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
@@ -28,6 +30,8 @@ export class HomeComponent {
   public cols = 3; // default
   public rowHeight = ROWS_HEIGHT[this.cols];
   public category: string | undefined;
+
+  private cartService = inject(CartService);
 
   onColumnsCountChange(colsNum: number) {
     this.cols = colsNum;
@@ -38,5 +42,15 @@ export class HomeComponent {
   onShowCategory(newCategory: string) {
     this.category = newCategory;
     console.log(this.category);
+  }
+
+  onAddToCart(product: ProductInterface) {
+    this.cartService.addToCart({
+      product: product.image,
+      name: product.title,
+      price: product.price,
+      quantity: 1,
+      id: product.id,
+    });
   }
 }
